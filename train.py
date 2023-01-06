0b0a85a4db5ea296ded545a9b3f7e2fa58914510
markcrowley1 <markcrowley1999@gmail.com>
2023-01-06 23:34:38 +0000

    Implemented example bpa algorithm script.

---
+++ train.py
@@ -0,0 +1,19 @@
+"""
+Description:
+    Initial script for training and experimenting with point set gen based network
+"""
+import keras
+import tensorflow as tf
+import cv2
+import pandas as pd
+
+def main():
+    """Load and format shapenet dataset"""
+    """Load and format Pascal3d+ dataset"""
+    """Build network according to point set gen architecture"""
+    """Train network"""
+    """Test network"""
+    pass
+
+if __name__ == "__main__":
+    main()

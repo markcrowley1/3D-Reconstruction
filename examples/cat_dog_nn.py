cec87e847155ee68b747a5954f49781d719f0d73
markcrowley1 <markcrowley1999@gmail.com>
2023-01-19 17:50:34 +0000

    updated example

--- examples/cat_dog_nn.py
+++ examples/cat_dog_nn.py
@@ -17,11 +17,14 @@ def main():
     X = x/255.0
     y = np.array(y)
 
+    print(X.shape[1:])
+    print(y[1])
+
     # Define model
     model = models.Sequential()
 
     # Input layer
-    model.add(layers.Conv2D(64, (3,3), activation="relu" ,input_shape=X.shape[1:]))
+    model.add(layers.Conv2D(64, (3,3), activation="relu" , input_shape=X.shape[1:]))
     model.add(layers.MaxPooling2D(pool_size=(2,2)))
 
     # Hidden Layer
@@ -39,8 +42,8 @@ def main():
         optimizer="adam",
         metrics=["accuracy"])
     
-    model.fit(X, y, batch_size=32, epochs=3, validation_split=0.1)
-    model.save("examples/cat_dog_classifier.model")
+    model.fit(X, y, batch_size=32, epochs=10, validation_split=0.1)
+    model.save("examples/cat_dog_classifier.keras")
 
 if __name__ == "__main__":
     main()

d24711a0b2edd93d8b27caf402f2b19b2b67fca9
markcrowley1 <markcrowley1999@gmail.com>
2023-02-16 14:35:24 +0000

    developed deeper psg network and implemented rough point cloud to mesh conversion

--- train.py
+++ train.py
@@ -70,10 +70,18 @@ def main():
     model.add(layers.Conv2D(16, (3,3), activation="relu", input_shape=train_imgs.shape[1:]))
     model.add(layers.Conv2D(16, (3,3), activation="relu"))
     model.add(layers.Conv2D(16, (3,3), activation="relu"))
+    
+    model.add(layers.Conv2D(32, (3,3), activation="relu"))
+    model.add(layers.Conv2D(32, (3,3), activation="relu"))
+    model.add(layers.Conv2D(32, (3,3), activation="relu"))
+
+    model.add(layers.Conv2D(64, (3,3), activation="relu"))
+    model.add(layers.Conv2D(64, (3,3), activation="relu"))
+    model.add(layers.Conv2D(64, (3,3), activation="relu"))
 
     # Add Fully Connected Layer
     model.add(layers.Flatten())
-    model.add(layers.Dense(32))
+    model.add(layers.Dense(64))
     model.add(layers.Dense(POINTS*3))
 
     # Reshape to required output
